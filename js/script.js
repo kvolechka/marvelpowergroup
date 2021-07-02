c71225dc101a8f92b3902e9b4f2e2cc8837ff681
Anastasia <71756913+kvolechka@users.noreply.github.com>
2021-07-02 13:41:35 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@
 const burgerMenuBtn = document.querySelector('.menu__btn');
 const burger = burgerMenuBtn.querySelector('span');
 const menuBox = document.querySelector('.menu__box');
+const menuClose = document.querySelector('.menu__list-link');
 
-burgerMenuBtn.addEventListener('click' , () => {
+burgerMenuBtn.addEventListener('click', () => {
     burgerMenuBtn.classList.toggle('active');
     burger.classList.toggle('active');
     menuBox.classList.toggle('active');
+});
+
+const burgerLinksBox = document.querySelector('.menu__box .links');
+burgerLinksBox.addEventListener('click', (event) => {
+    if(event.target.classList.contains('menu__list-link')) {
+        burgerMenuBtn.classList.remove('active');
+        burger.classList.remove('active');
+        menuBox.classList.remove('active');
+    }
 });
 
 const formBtn = document.querySelector('.footer__form-send');
